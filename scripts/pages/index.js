3c0a8b00d7f48aa31ac68653ef363ec49fce576b
Valène Renaud <valene.renaud@gmail.com>
2024-06-09 12:42:37 +0200

    feat(homepage): implement data fetching from JSON file and display photographers

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,58 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+import { PhotographerTemplate } from "../templates/photographer.js";
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
-
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerTemplate(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
+/**
+ * Récupére les données des photographes à partir d'un fichier JSON
+ * @returns {Promise<Array>} Une promesse qui retourne un tableau de photographes si la requête réussit
+ */
+async function getPhotographers() {
+    try {
+        // Effectue une requête HTTP GET pour obtenir les données
+        const response = await fetch('data/photographers.json');
+        const data = await response.json();
+        return data.photographers;  
+    } catch (error) {
+        // Si échec de la requête, enregistre l'erreur dans la console
+        console.error("Erreur lors du chargement des données des photographes", error);
+        return [];  // Retourne un tableau vide en cas d'erreur
     }
+}
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    }
-    
-    init();
+
+/**
+ * Affiche les données des photographes dans le DOM
+ * @param {Array} photographers Un tableau d'objets représentant les photographes
+ */
+async function displayData(photographers) {
+    const photographersSection = document.querySelector(".photographer_section");
+
+    // Utilise un DocumentFragment pour minimiser les réécritures dans le DOM
+    // Crée un DocumentFragment pour regrouper les éléments DOM avant de les attacher au document
+    const fragment = document.createDocumentFragment();
+
+    // Boucle sur chaque photographe et ajoute son élément DOM au fragment
+    photographers.forEach((photographer) => {
+         // Instancie la classe PhotographerTemplate avec les données du photographe
+        const photographerModel = new PhotographerTemplate(photographer);
+         // Crée l'élément DOM pour le photographe en utilisant la méthode définie dans la classe PhotographerTemplate
+        const userCardDOM = photographerModel.getUserCardDOM();
+        // Ajoute l'élément du photographe au fragment, pas directement au DOM principal
+        fragment.appendChild(userCardDOM);
+    });
+
+    // Ajoute le fragment complet à la section des photographes dans le DOM principal: permet de minimiser les modifications du DOM et optimise les performances
+    photographersSection.appendChild(fragment);
+}
+
+
+/**
+ * Initialise pour charger les données et mettre à jour le DOM
+ */
+async function init() {
+    // Récupère les données des photographes
+    const photographers = await getPhotographers();
+    displayData(photographers);
+}
+ 
+// Appelle la fonction init lors du chargement du script
+init();
     
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,52 @@
-function photographerTemplate(data) {
-    const { name, portrait } = data;
+/**
+ * Représente un photographe
+ */
+export class PhotographerTemplate {
+    constructor(data) {
+        // Initialise l'instance avec les données fournies
+        const { name, portrait, city, country, tagline, price } = data;
+        this.name = name;
+        this.portrait = portrait;
+        this.city = city;
+        this.country = country;
+        this.tagline = tagline;
+        this.price = price;
+        this.picture = `assets/photographers/idPhotos/${portrait}`;
+    }
 
-    const picture = `assets/photographers/${portrait}`;
+    /**
+     * Crée et retourne un élément DOM représentant le photographe
+     * @returns {HTMLElement} L'élément "article" contenant les détails du photographe
+     */
+    getUserCardDOM() {
+        const article = document.createElement("article");
+        const img = document.createElement("img");
+        img.setAttribute("src", this.picture);
+        img.setAttribute("alt", `Portrait de ${this.name}`); // Ajoute un attribut "alt" pour l'accessibilité
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
+        const h2 = document.createElement("h2");
+        h2.textContent = this.name;
+
+        const info = document.createElement("div");
+        info.className = "photographer-info";
+
+        // Concaténation de la ville et du pays dans le même élément <p>
+        const locationInfo = document.createElement("p");
+        locationInfo.textContent = `${this.city}, ${this.country}`; 
+        info.appendChild(locationInfo);
+
+        const taglineInfo = document.createElement("p");
+        taglineInfo.textContent = this.tagline;
+        info.appendChild(taglineInfo);
+
+        const priceInfo = document.createElement("p");
+        priceInfo.textContent = `${this.price}€/jour`;
+        info.appendChild(priceInfo);
+
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        article.appendChild(info);
+
+        return article;
     }
-    return { name, picture, getUserCardDOM }
 }
